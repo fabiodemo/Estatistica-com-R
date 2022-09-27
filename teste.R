b3073948b80a3b37584f2207f96c428db30e7978
fabiodemo <faberdemo@gmail.com>
2022-09-26 22:50:43 -0300

    Testando ggplot

--- teste.R
+++ teste.R
@@ -1,3 +1,14 @@
+library(ggplot2)
+
 View(iris)
 
-library(ggplot2)
+ToothGrowth$dose <- as.factor(ToothGrowth$dose)
+mtcars$cyl <- as.factor(mtcars$cyl)
+head(ToothGrowth)
+
+# Box plot
+p <- ggplot(ToothGrowth, aes(x=dose, y=len)) +geom_boxplot()
+print(p)
+# scatter plot
+p2 <- ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
+print(p2)
