298c4c08a4c1fa25a55565c8957403b01ff91804
fabiodemo <faberdemo@gmail.com>
2022-09-30 20:28:42 -0300

    FInalização de importação de dataframes

--- Introdução/importacao_data_frames.R
+++ Introdução/importacao_data_frames.R
@@ -9,3 +9,26 @@ setwd("Introdução/files")
 df1 <- read.table("partks.txt")
 df1
 View(df1)
+
+# Leitura de csv
+
+df2 <- read.csv("mola.csv")
+View(df2)
+
+df3 <- read.csv("questoes.csv")
+View(df3)
+
+df3 <- read.csv("questoes.csv", encoding= "latin-1")
+
+df3 <- read.csv("questoes.csv", encoding= "iso-8859-1")
+
+df3 <- read.csv("questoes.csv", encoding= "UTF-8")
+
+# Arquivo Excel
+
+install.packages("readxl")
+library(readxl)
+
+df4 <- read_excel("registro.xlsx")
+df4
+View(df4)
