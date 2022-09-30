e8012b897fff9bfae78e9a5fe31655287bdd9f2a
fabiodemo <faberdemo@gmail.com>
2022-09-30 20:24:19 -0300

    Carregamento de arquivos

---
+++ Introdução/importacao_data_frames.R
@@ -0,0 +1,11 @@
+#####################################
+###   IMPORTAÇÃO DE DATA FRAMES   ###
+#####################################
+
+setwd("Introdução/files")
+
+# Arquivo txt
+
+df1 <- read.table("partks.txt")
+df1
+View(df1)
