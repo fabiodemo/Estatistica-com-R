8a9347dd97f1ba5835da376915f8445caad04e42
fabiodemo <faberdemo@gmail.com>
2022-10-06 19:55:22 -0300

    Tipos de atributos, NA, e NaN

--- II-Estruturação/enem_rs_2019.R
+++ II-Estruturação/enem_rs_2019.R
@@ -32,3 +32,25 @@ enem_rs_2019 <- rename(enem_rs_2019, NOTA_COMP1 = NU_NOTA_COMP1)
 # Renomear múltipla colunas
 enem_rs_2019 <- rename(enem_rs_2019, NOTA_COMP2 = NU_NOTA_COMP2, NOTA_COMP3 = NU_NOTA_COMP3, NOTA_COMP4 = NU_NOTA_COMP4, NOTA_REDACAO = NU_NOTA_REDACAO, NOTA_CN = NU_NOTA_CN, NOTA_CH = NU_NOTA_CH, NOTA_LC = NU_NOTA_LC, NOTA_MT = NU_NOTA_MT)
 View(enem_rs_2019)
+
+# Verificação de tipagem dos atributos (Variáveis)
+# 6 Tipos básicos:
+# character
+# integer
+# numeric
+# logical
+# complex
+# factor
+
+str(enem_rs_2019)
+#OU
+glimpse(enem_rs_2019)
+
+# Transformando a variável Código escola em fator
+enem_rs_2019$CO_ESCOLA <- as.factor(enem_rs_2019$CO_ESCOLA)
+
+# Verificando valores ausentes
+# NA Not Available
+# NaN Not a Number
+sapply(enem_rs_2019, function(x) sum(is.na(x)))
+sapply(enem_rs_2019, function(x) sum(is.nan(x)))
