941ebe6e92fa1364bf67a35e8c02ee433c1f5d4d
fabiodemo <faberdemo@gmail.com>
2022-10-22 08:57:18 -0300

    Organização do diretório sobre amostragem

---
+++ III-Conceitos/amostragem_sistematica.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(sampling)
+library(TeachingSampling)
+
+setwd("III-Conceitos/dados/")
+# setwd("II-Estruturação/dados/")
+
+enem_2019_tratado <- read.csv("enem_rs_2019_tratado.csv")
+View(enem_2019_tratado)
