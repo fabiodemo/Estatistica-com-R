99bb423d3864835a2e94d8571bca5bcaa026616d
fabiodemo <faberdemo@gmail.com>
2023-02-12 15:54:08 -0300

    Condição para instalar os pacotes

--- III-Conceitos/amostragem_sistematica.R
+++ III-Conceitos/amostragem_sistematica.R
@@ -1,3 +1,7 @@
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(sampling)) install.packages("dplyr")
+if(!require(TeachingSampling)) install.packages("dplyr")
+
 library(dplyr)
 library(sampling)
 library(TeachingSampling)
@@ -7,3 +11,7 @@ setwd("III-Conceitos/dados/")
 
 enem_2019_tratado <- read.csv("enem_rs_2019_tratado.csv")
 View(enem_2019_tratado)
+
+set.seed(2)
+amostra <- S.SY(1199,100)
+amostra
