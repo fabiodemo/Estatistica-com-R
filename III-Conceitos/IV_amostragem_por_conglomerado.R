c012998da6c1a2efa60c6b222b5b7bb73d9d45e6
fabiodemo <faberdemo@gmail.com>
2023-02-12 17:14:34 -0300

    Inicialização da Amostragem por Conglomerado

---
+++ III-Conceitos/IV_amostragem_por_conglomerado.R
@@ -0,0 +1,21 @@
+if(!require(dplyr)) install.packages("dplyr", dependencies=True)
+if(!require(sampling)) install.packages("sampling",  dependencies=True)
+if(!require(TeachingSampling)) install.packages("TeachingSampling", dependencies=True)
+
+library(dplyr)
+library(sampling)
+library(magrittr)
+library(TeachingSampling)
+
+setwd("III-Conceitos/dados/")
+# setwd("II-Estruturação/dados/")
+
+enem_2019_tratado <- read.csv("enem_rs_2019_tratado.csv")
+View(enem_2019_tratado)
+
+#### AMOSTRAGEM ESTRATIFICADA ####
+# Quando dividimos uma população em grupos e depois pegamos uma amostra de cada um desses grupos.
+
+# Amostragem em Santa Maria
+enem_poa <- enem_2019_tratado %>% filter(NO_MUNICIPIO_RESIDENCIA=="Porto Alegre")
+enem_poa
