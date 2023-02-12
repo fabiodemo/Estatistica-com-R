81ed1f0422699347f9c40fd24e60f7b98a310dec
fabiodemo <faberdemo@gmail.com>
2023-02-12 16:22:14 -0300

    Correção nas dependencias

--- III-Conceitos/amostragem_sistematica.R
+++ III-Conceitos/amostragem_sistematica.R
@@ -1,9 +1,10 @@
-if(!require(dplyr)) install.packages("dplyr")
-if(!require(sampling)) install.packages("dplyr")
-if(!require(TeachingSampling)) install.packages("dplyr")
+if(!require(dplyr)) install.packages("dplyr", dependencies=True)
+if(!require(sampling)) install.packages("sampling",  dependencies=True)
+if(!require(TeachingSampling)) install.packages("TeachingSampling", dependencies=True)
 
 library(dplyr)
 library(sampling)
+library(magrittr)
 library(TeachingSampling)
 
 setwd("III-Conceitos/dados/")
@@ -12,6 +13,10 @@ setwd("III-Conceitos/dados/")
 enem_2019_tratado <- read.csv("enem_rs_2019_tratado.csv")
 View(enem_2019_tratado)
 
+
+enem_poa <- enem_2019_tratado %>% filter (NO_MUNICIPIO_RESIDENCIA=="Porto Alegre")
+enem_poa
+
 set.seed(2)
-amostra <- S.SY(1199,100)
+amostra <- S.SY(35841, 100)
 amostra
