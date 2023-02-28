e03247a5027e93be3ead1b7d38ef7f7a5a5096e3
user.name <faberdemo@gmail.com>
2023-02-27 21:03:50 -0300

    Inicialização do diretõrio

---
+++ IV-Estatistica-Descritiva/I_distribuicao_de_frequencias.R
@@ -0,0 +1,12 @@
+#######################################
+##### DISTRIBUIÇÃO DE FREQUÊNCIAS #####
+#######################################
+
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(sampling)) install.packages("sampling")
+if(!require(TeachingSampling)) install.packages("TeachingSampling")
+
+library(dplyr)
+library(sampling)
+library(TeachingSampling)
+
