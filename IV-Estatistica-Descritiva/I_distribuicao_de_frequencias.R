e0f62281b670f9779bee8ad500c8af1bd10eb09e
user.name <faberdemo@gmail.com>
2023-02-28 08:32:48 -0300

    Finalização da Distribuição de Frequências

--- IV-Estatistica-Descritiva/I_distribuicao_de_frequencias.R
+++ IV-Estatistica-Descritiva/I_distribuicao_de_frequencias.R
@@ -10,3 +10,41 @@ library(dplyr)
 library(sampling)
 library(TeachingSampling)
 
+getwd()
+setwd('IV-Estatistica-Descritiva')
+enem2019_tratado <- read.csv('enem_sp_2019.csv', sep=',')
+
+enem_sorocaba <- enem2019_tratado %>% filter(NO_MUNICIPIO_RESIDENCIA=="Sorocaba")
+
+# Tabela de Frequências Absolutas
+freq_abs <- table(enem_sorocaba$NU_IDADE)
+View(freq_abs)
+freq_abs
+
+# Tabela de Frequências Relativas
+freq_rel <- prop.table(freq_abs)
+View(freq_abs)
+
+# Porcentagem de Frequência Relativas
+p_freq_rel <- 100 * prop.table(freq_abs)
+View(p_freq_rel)
+
+# Criar uma linha com o total
+freq_abs <- c(freq_abs, sum(freq_abs))
+View(freq_abs)
+names(freq_abs)[80] <- "Total"
+View(freq_abs)
+
+# Juntando a Frequência Relativa e a Frequência Percentual com suas respectivas somas
+freq_rel <- c(freq_rel, sum(freq_rel))
+p_freq_rel <- c(p_freq_rel, sum(p_freq_rel))
+
+# Tabela final com todos os valores
+tabela_final <- cbind(freq_abs, freq_rel=round(freq_rel, digits=5), p_freq_rel=round(p_freq_rel, digits=2))
+View(tabela_final)
+
+# Construindo classes de Frequências
+intervalo_classes <- seq(10, 75, 5)
+View(intervalo_classes)
+tabela_classes <- table(cut(enem_sorocaba$NU_IDADE, breaks=intervalo_classes, right=FALSE))
+View(tabela_classes)
