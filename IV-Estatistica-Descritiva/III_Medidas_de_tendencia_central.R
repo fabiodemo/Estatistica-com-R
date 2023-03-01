4b262e03c890f1397dbbfbbdc19003cd4543a970
user.name <faberdemo@gmail.com>
2023-02-28 21:10:55 -0300

    Inicialização das Medidas de Tendência Central

---
+++ IV-Estatistica-Descritiva/III_Medidas_de_tendencia_central.R
@@ -0,0 +1,17 @@
+###################################
+##### MEDIDAS DE TENDÊNCIA CENTRAL #####
+###################################
+
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(sampling)) install.packages("sampling")
+if(!require(TeachingSampling)) install.packages("TeachingSampling")
+
+library(dplyr)
+library(sampling)
+library(TeachingSampling)
+
+getwd()
+setwd('IV-Estatistica-Descritiva')
+enem2019_tratado <- read.csv('enem_sp_2019.csv', sep=',')
+
+enem_sorocaba <- enem2019_tratado %>% filter(NO_MUNICIPIO_RESIDENCIA=="Sorocaba")
